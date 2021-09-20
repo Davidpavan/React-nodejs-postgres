846acc973228ddfb71f614173dd569aa18991eec
srikavya23 <82562508+srikavya23@users.noreply.github.com>
2021-09-20 23:02:40 +0530

    Update db.config.js

--- backend/app/config/db.config.js
+++ backend/app/config/db.config.js
@@ -1,14 +1,17 @@
+const env = require('./env.js');
+require('dotenv').config();
+ 
 const Sequelize = require('sequelize');
-const sequelize = new Sequelize(process.env.database, process.env.username, process.env.password, {
+const sequelize = new Sequelize(env.database, env.username, env.password, {
   host: env.host,
   dialect: env.dialect,
   operatorsAliases: false,
  
   pool: {
     max: env.max,
-    min: env.pool.min,
-    acquire: env.pool.acquire,
-    idle: env.pool.idle
+    min: env.min,
+    acquire: env.acquire,
+    idle: env.idle
   }
 });
 const db = {};
