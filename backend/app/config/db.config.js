1400a490fc4161ced4d0720d73c959c0b5c40e09
srikavya23 <82562508+srikavya23@users.noreply.github.com>
2021-09-20 22:29:50 +0530

    Update db.config.js

--- backend/app/config/db.config.js
+++ backend/app/config/db.config.js
@@ -1,6 +1,6 @@
 const Sequelize = require('sequelize');
 const sequelize = new Sequelize(process.env.database, process.env.username, process.env.password, {
-  host: process.env.host,
+  host: env.host,
   dialect: env.dialect,
   operatorsAliases: false,
  
